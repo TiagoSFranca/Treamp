b25f8bc69ef7b50b405256d6c8266f6e6a37bb74
tiago <tiago@LAPTOP-DL1DDPST>
2017-06-15 11:29:24 -0300

    Ajustes par exibir o total dos gastos pessoais.

--- Projeto/Presentation/Models/ViewModels/TravelViewModel.cs
+++ Projeto/Presentation/Models/ViewModels/TravelViewModel.cs
@@ -15,6 +15,7 @@ namespace Presentation.Models.ViewModels
         public List<DestinationViewModel> Destinations { get; set; }
         public List<TravelUserViewItem> TravelUsers { get; set; }
         public List<CostViewModel> PersonalCosts { get; set; }
+        public double TotalPersonal { get; set; }
         public List<TravelUserCostViewModel> GroupCosts { get; set; }
     }
 
