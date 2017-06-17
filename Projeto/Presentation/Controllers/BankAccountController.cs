b04751afcc4987f3614040d813f18b064896f19d
tiago <tiago@LAPTOP-DL1DDPST>
2017-06-17 17:03:16 -0300

    Ajustes para inserir e listar as contas bancárias.

--- Projeto/Presentation/Controllers/BankAccountController.cs
+++ Projeto/Presentation/Controllers/BankAccountController.cs
@@ -24,12 +24,50 @@ namespace Presentation.Controllers
         [HttpPost]
         public ActionResult AddAccount(BankAccountViewModel bank)
         {
+            messageModel.Title = "Cadastrar Conta";
+
+            userLogged = (UserViewItem)HttpContext.Session["user"];
+            bank.IdUser = userLogged.Id;
+            if(VerifyAccountExists(bank) != null)
+            {
+                ModelState.AddModelError("Account", "Conta já existe");
+            }
+
             if (!ModelState.IsValid)
             {
                 FulFillLists(bank);
                 return View("_AddAccount", bank);
             }
-            return View("_AddAccount", bank);
+            if (!CanCreateAccount(userLogged.Id))
+            {
+                messageModel.Message = "Você já possui o máximo de contas permitidas: 2";
+            }else
+            {
+                try
+                {
+                    var bankMapped = AutoMapper.Mapper.Map<BankAccountViewModel, BankAccount>(bank);
+                    db.BankAccount.Add(bankMapped);
+                    db.SaveChanges();
+                    messageModel.Message = "Conta bancária adicionada com sucesso!";
+                }
+                catch (Exception e)
+                {
+                    messageModel.Message = "Não foi possível concluir sua solicitação!\n Tente novamente mais tarde.";
+                }
+            }
+            return View("_Message", messageModel);
+        }
+
+        private BankAccount VerifyAccountExists(BankAccountViewModel bank)
+        {
+            var acc = db.BankAccount.Where(b => b.Account == bank.Account && b.Agency == bank.Agency && b.Name == bank.Name).FirstOrDefault();
+            return acc;
+        }
+
+        private bool CanCreateAccount(int idUser)
+        {
+            var countAcc = db.BankAccount.Where(b => b.IdUser == idUser).Count();
+            return (countAcc < 2);
         }
 
         private void FulFillLists(BankAccountViewModel bank)
